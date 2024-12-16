bac15b70685968f307eda6c22c48d221eadbd59d
EJ Perez <ej.perez@roiroi.com>
2024-12-16 21:45:41 +0800

    Change to dark theme, change workflow listing layout, sort by date

--- plugin/scripts.js
+++ plugin/scripts.js
@@ -1,126 +1,134 @@
 
 const config = {
-	token: '',
-	base: 'https://api.github.com',
+	token: "",
+	base: "https://api.github.com",
 	maxPage: 3,
-	owner: 'stokmedia',
-	deploybotToken: '',
-	deploybotBaseURL: 'https://stokmedia.deploybot.com/api/v1',
-	deploybotPublicURL: 'https://stokmedia.deploybot.com'
+	owner: "stokmedia",
+	deploybotToken: "",
+	deploybotBaseURL: "https://stokmedia.deploybot.com/api/v1",
+	deploybotPublicURL: "https://stokmedia.deploybot.com"
 };
 
 const refreshWorkflows = () => {
-	const currentRepo = document.getElementById( 'js-repos' ).value;
+	const currentRepo = document.getElementById("js-repos").value;
 
-	chrome.storage.local.set( { 'current_repo': currentRepo }, function () {
-		chrome.runtime.sendMessage( { config: config, event: 'getWorkflows', currentRepo: currentRepo } )
-			.then( response => {
-				renderWorkflows( response );
-			} );
-	} );
+	const loader = document.getElementById("js-loading"),
+		workflowsContainer = document.getElementById("js-workflows");
+
+	loader.classList.remove("d-none");
+	workflowsContainer.classList.add("d-none");
+
+	chrome.storage.local.set({ "current_repo": currentRepo }, function () {
+		chrome.runtime.sendMessage({ config: config, event: "getWorkflows", currentRepo: currentRepo })
+			.then(response => {
+				renderWorkflows(response);
+				loader.classList.add("d-none");
+				workflowsContainer.classList.remove("d-none");
+			});
+	});
 };
 
-const renderWorkflows = ( workflows ) => {
-	document.getElementById( 'js-workflows' ).innerHTML = workflows.map( workflow => {
-		let html = '<tr><td>' + workflow.host + '</td><td>' + workflow.name + '</td>';
+const renderWorkflows = (workflows) => {
+	document.getElementById("js-workflows").innerHTML = workflows.filter(workflow => workflow.lastRun)
+		.sort((a, b) => {
+			if (a.lastRun.timestamp > b.lastRun.timestamp) {
+				return 1;
+			} else if (a.lastRun.timestamp > b.lastRun.timestamp) {
+				return -1;
+			}
 
-		if ( workflow.lastRun ) {
-			const timestamp = workflow.lastRun.timestamp ? moment( workflow.lastRun.timestamp ).fromNow() : '(date not available)';
+			return 0;
+		})
+		.map(workflow => {
+			const timestamp = workflow.lastRun.timestamp ? moment(workflow.lastRun.timestamp).fromNow() : "(date not available)",
+				newName = workflow.name.replaceAll("Deployment - ", "");
 
-			html += '<td>' + workflow.lastRun.branch + '</td>' +
-				'<td>' + workflow.lastRun.commit + '</td>' +
-				'<td>' + workflow.lastRun.author + '</td>' +
-				'<td>' + workflow.lastRun.actor + '</td>' +
-				'<td>' + timestamp + '</td>' +
-				'<td><a href="' + workflow.link + '" target="_blank">Deploy</a></td>';
-		} else {
-			html += '<td colspan="5" class="text-center">No workflow runs</td>' +
-				'<td><a href="' + workflow.link + '" target="_blank">Deploy</a></td>';
-		}
-
-		html += '</tr>';
-
-		return html;
-	} ).join( '' );
+			return `<a href="${workflow.link}" class="list-group-item list-group-item-action overflow-x-hidden text-nowrap" title="View deployment">
+				<span class="fw-bold">${newName}</span>						
+				<span class="badge text-bg-primary">${workflow.lastRun.branch}</span>
+				<span>Ran by <span class="fst-italic">${workflow.lastRun.actor}</span> <span class="fw-bold">${timestamp}</span></span>
+				<div class="fst-italic">${workflow.lastRun.commit}</div>
+			</a>`;
+		}).join("");
 };
 
 const renderRepoList = () => {
-	chrome.storage.local.get( ['repos', 'current_repo'], function ( items ) {
+	chrome.storage.local.get(["repos", "current_repo"], function (items) {
 		let repos = items.repos,
 			currentRepo = items.current_repo;
 
-		if ( !repos ) {
+		if (!repos) {
 			return;
 		}
 
-		document.getElementById( 'js-repos' ).innerHTML = '<option value="" disabled selected>Select a repo</option>' + repos
-			.map( repo => {
-				return `<option ${currentRepo === repo.value ? 'selected' : ''} value="${repo.value}">${repo.name} (${repo.type})</option>`
-			} )
-			.join( '' );
+		document.getElementById("js-repos").innerHTML = '<option value="" disabled selected>Select a repo</option>' + repos
+			.map(repo => {
+				return `<option ${currentRepo === repo.value ? "selected" : ""} value="${repo.value}">${repo.name} (${repo.type})</option>`
+			})
+			.join("");
 
-		if ( currentRepo ) {
-			refreshWorkflows( currentRepo );
+		if (currentRepo) {
+			refreshWorkflows(currentRepo);
 		}
-	} );
+	});
 };
 
 const checkSettings = () => {
-	chrome.storage.local.get( ['github_token', 'deploybot_token', 'repos'], function ( items ) {
-		document.getElementById( 'js-token' ).value = items.github_token && items.deploybot_token ? items.github_token + '|' + items.deploybot_token : '';
+	chrome.storage.local.get(["github_token", "deploybot_token", "repos"], function (items) {
+		document.getElementById("js-token").value = items.github_token && items.deploybot_token ? items.github_token + "|" + items.deploybot_token : "";
 		config.token = items.github_token;
 		config.deploybotToken = items.deploybot_token;
 
-		if ( config.token ) {
-			document.getElementById( 'js-repos-container' ).classList.remove( 'd-none' );
-			document.getElementById( 'js-no-repos-container' ).classList.add( 'd-none' );
+		if (config.token) {
+			document.getElementById("js-repos-container").classList.remove("d-none");
+			document.getElementById("js-no-repos-container").classList.add("d-none");
 		} else {
-			document.getElementById( 'js-repos-container' ).classList.add( 'd-none' );
-			document.getElementById( 'js-no-repos-container' ).classList.remove( 'd-none' );
+			document.getElementById("js-repos-container").classList.add("d-none");
+			document.getElementById("js-no-repos-container").classList.remove("d-none");
 		}
-	} );
+	});
 };
 
 const saveSettings = () => {
-	const token = document.getElementById( 'js-token' ).value;
-	const tokenSplitted = token.split( '|' );
+	const token = document.getElementById("js-token").value;
+	const tokenSplitted = token.split("|");
 
 	config.token = tokenSplitted[0];
 	config.deploybotToken = tokenSplitted[1];
 
-	chrome.storage.local.set( { 'github_token': tokenSplitted[0], 'deploybot_token': tokenSplitted[1] }, function () {
-		showMesage( 'Settings saved!' );
+	chrome.storage.local.set({ "github_token": tokenSplitted[0], "deploybot_token": tokenSplitted[1] }, function () {
+		showMesage("Settings saved!");
 		checkSettings();
-	} );
+	});
 };
 
 const refreshRepoCache = () => {
-	chrome.runtime.sendMessage( { config: config, event: 'refreshRepoCache' } )
-		.then( response => {
-			chrome.storage.local.set( { repos: response }, () => {
-				showMesage( 'Repo cache updated!' );
+	chrome.runtime.sendMessage({ config: config, event: "refreshRepoCache" })
+		.then(response => {
+			chrome.storage.local.set({ repos: response }, () => {
+				showMesage("Repo cache updated!");
 				renderRepoList();
-			} );
-		} );
+			});
+		});
 };
 
-const showMesage = ( message ) => {
-	document.getElementById( 'js-message' ).innerText = message;
+const showMesage = (message) => {
+	document.getElementById("js-message").innerText = message;
 
-	setTimeout( () => {
-		document.getElementById( 'js-message' ).innerText = '';
-	}, 2000 );
+	setTimeout(() => {
+		document.getElementById("js-message").innerText = "";
+	}, 2000);
 };
 
 window.onload = () => {
 	checkSettings();
 	renderRepoList();
 
-	const repos = document.getElementById( 'js-repos' );
+	const repos = document.getElementById("js-repos");
 
-	repos.addEventListener( 'change', refreshWorkflows );
+	repos.addEventListener("change", refreshWorkflows);
 	repos.focus();
 
-	document.getElementById( 'js-refresh-repos' ).addEventListener( 'click', refreshRepoCache );
-	document.getElementById( 'js-save-settings' ).addEventListener( 'click', saveSettings );
+	document.getElementById("js-refresh-repos").addEventListener("click", refreshRepoCache);
+	document.getElementById("js-save-settings").addEventListener("click", saveSettings);
 }
